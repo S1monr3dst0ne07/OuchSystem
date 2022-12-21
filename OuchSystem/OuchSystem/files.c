fe9873f0d64cc95ef6d59631512145405af02184
S1mon <jaforsi.kozak@gmail.com>
2022-12-21 14:58:33 +0100

    implemented new image format

--- OuchSystem/OuchSystem/files.c
+++ OuchSystem/OuchSystem/files.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include "files.h"
 
 struct fileNode
 {
@@ -7,7 +10,7 @@ struct fileNode
 	int prior;
 
 	//only used if type == 0
-	struct fileNodeList* dirs;
+	struct nodeList* subNodes;
 
 	//only used if type == 1
 	char* content;
@@ -17,42 +20,86 @@ struct fileNode
 
 
 };
-struct fileNodeList
+struct nodeList
 {
 	struct fileNode* content;
-	struct fileNodeList* next;
+	struct nodeList* next;
+	bool isEnd;
 };
+
+struct fileNode* allocFileNode()
+{
+	return (struct fileNode*)malloc(sizeof(struct fileNode*));
+}
+struct nodeList* allocNodeList()
+{
+	return (struct nodeList*)malloc(sizeof(struct nodeList*));
+}
 
 
 
 
 //mounts file system from host system image file
-struct dir* mountRootImage(char* path)
+struct fileNode* mountRootImage(char* path)
 {
 	FILE* fp = fopen(path, "r");
 
 
 }
 
-struct dir* parseNode(FILE* fp)
-{
-	
 
+//parses node, without 0x10 header
+struct fileNode* parseNode(char* ptr)
+{
+	char* name  = readTermed(ptr);
+	char* prior = readTermed(ptr);
+
+	struct fileNode* newNode = allocFileNode();
+	newNode->name = name;
+	newNode->prior = (int)(*prior);
+	newNode->subNodes = NULL;
+
+	int type = (int)(*(ptr++));
+	switch (type)
+	{
+	case 0x11:
+		newNode->type = 1;
+
+		//init linked list
+		struct nodeList* listHead = allocNodeList();
+		newNode->subNodes = listHead;
+
+		while (*(ptr++) != imageTermi)
+		{
+			struct nodeList* listNew = allocNodeList();
+			listHead->content = parseNode(ptr);
+			listHead->next = listNew;
+			listHead = listNew;
+		}
+
+		listHead->isEnd = true;
+		break;
+
+
+	case 0x12:
+		newNode->type = 2;
+		newNode->content = readTermed(ptr);
+		break;
+	}
+
+	return newNode;
 }
 
-//reads 0x00 terminated string from file ptr
-char* readContent(FILE* fp)
+//reads 0x01 terminated string
+char* readTermed(char* ptr)
 {
-	//get length of string
-	FILE* temp = fp;
-	int strLen = 0;
-	while (fgetc(temp)) strLen++;
+	char* temp = ptr;
+	int len = 0;
+	while (*(temp++) != imageTermi) len++;
 
-	char* output = (char*)malloc(sizeof(char) * strLen);
-
-	//read string
+	char* output = (char*)malloc(len * sizeof(char*));
 	int i = 0;
-	while (output[i++] = fgetc(temp));
-
+	while (*(ptr++) != imageTermi) output[i++] = *(ptr-1);
+	
 	return output;
 }
--- OuchSystem/OuchSystem/files.h
+++ OuchSystem/OuchSystem/files.h
@@ -1,7 +1,14 @@
+#ifndef HFILES
+#define HFILES
 
+#define imageTermi 0x01
+#define imageNode  0x10
 
 struct fileNode;
-struct fileNodeList;
+struct nodeList;
 
-struct dir* mountRootImage(char* path);
+struct fileNode* mountRootImage(char* path);
+struct fileNode* parseNode(char* ptr);
+char* readTermed(char* ptr);
 
+#endif
