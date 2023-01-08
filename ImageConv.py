266128d58b8154e7a8e6a7bf6622163daedfebc0
S1mon <jaforsi.kozak@gmail.com>
2023-01-08 22:30:35 +0100

    started on ImageConv.py, renamed from File2Image.py

--- ImageConv.py
+++ ImageConv.py
@@ -7,8 +7,8 @@ def flat(l):
     if len(l) == 0: return []
     return functools.reduce(lambda x, y: x+y, l)
 
-class cFile:
-    def __init__(self, xHostPath):
+class cFile:   
+    def LoadHost(self, xHostPath):
         try:
             with open(xHostPath, "r") as xFilePtr:
                 self.xContent = xFilePtr.read()
@@ -27,7 +27,9 @@ class cFile:
             self.xName = xNameRaw
             self.xPrior = 0xff        
 
-    def GetBin(self):        
+        return self
+
+    def SaveImg(self):        
         xName = [ord(x) for x in self.xName]
         return [0x10, *xName, TERMI, self.xPrior, TERMI, 0x12, *[ord(x) for x in self.xContent], TERMI]
 
@@ -36,21 +38,21 @@ class cFile:
         
 
 class cNode:
-    def __init__(self, xHostPath):
+    def LoadHost(self, xHostPath):
         (xRoot, xSubdirs, xFiles) = next(os.walk(xHostPath))
         assert(xRoot == xHostPath)
         
-        self.xFiles   = [cFile(os.path.join(xHostPath, x)) for x in xFiles]
-        self.xSubDirs = [cNode(os.path.join(xHostPath, x)) for x in xSubdirs]
+        self.xFiles   = [cFile().LoadHost(os.path.join(xHostPath, x)) for x in xFiles]
+        self.xSubDirs = [cNode().LoadHost(os.path.join(xHostPath, x)) for x in xSubdirs]
 
         self.xName    = os.path.basename(xHostPath)
         self.xPrior = 0xff
         
-    def GetBin(self):       
+    def SaveImg(self):       
         
         xName = [ord(x) for x in self.xName]
-        xSubFiles = flat([x.GetBin() for x in self.xFiles])
-        xSubDirs  = flat([x.GetBin() for x in self.xSubDirs])
+        xSubFiles = flat([x.SaveImg() for x in self.xFiles])
+        xSubDirs  = flat([x.SaveImg() for x in self.xSubDirs])
         
         xSubNodes = xSubFiles + xSubDirs
         return [0x10, *xName, TERMI, self.xPrior, TERMI, 0x11, *xSubNodes, TERMI]
@@ -63,10 +65,40 @@ class cNode:
         
 
 
-xRoot = cNode(sys.argv[1])
-xRoot.Print()
+xRoot = cNode()
+
+while True:
+    try:
+        xInj = input(f"'{xRoot}' >>> ").strip().lower()
+        (xCom, xArgs*) = xInj.split(" ")
+        
+        if xCom == "clear":
+            xRoot = cNode()
+        
+        elif xCom == "print":
+            xRoot.Print()
+            
+        elif xCom == "loadhost":
+            xRoot.LoadHost(xArgs[0])
+            
+        elif xCom == "help":
+            print("""
+clear    - reset buffer
+print    - print buffer
+loadhost - load image from host filesystem
+
+            """)
+        
+        else:
+            print("Unknown injection")
+        
+
+    except Exception as E:
+        print(E)
+
+"""
 xBin = [x for x in xRoot.GetBin() if x < 0x100]
 
 with open("image.bin", "wb") as xFilePtr:
     xFilePtr.write(bytearray(xBin))
-
+"""
